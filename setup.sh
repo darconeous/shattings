ac6eaaa0af69ebbbb6785697d4effa0b4a8b4846
Robert Quattlebaum <darco@deepdarc.com>
2011-03-30 11:46:21 -0700

    More updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+HOMEDIR='~'
+SHATDIR="$HOMEDIR/.shattings"
+
 function die() {
 	echo $*
 	exit 1
@@ -7,7 +10,46 @@ function die() {
 
 set -x
 
-ln -s .shattings/profile ~/.profile
-ln -s .shattings/vim/vimrc ~/.vimrc
-ln -s .shattings/vim ~/.vim
+if [ -e $HOMEDIR/.bashrc ]
+then
+	{
+		echo '# Uncomment to enable'
+		echo '# '". \"$SHATDIR/bash/bashrc\""
+	} >> "${HOMEDIR}/.bashrc"
+	echo Added stub to existing $HOMEDIR/.bashrc
+else
+	{
+		echo ". \"$SHATDIR/bash/bashrc\""
+	} >> "${HOMEDIR}/.bashrc"
+	echo Created $HOMEDIR/.bashrc
+fi
+
+if [ -e "${HOMEDIR}/.vimrc" ]
+then
+	{
+		echo '" Uncomment to enable'
+		echo '" '"source $SHATDIR/vim/vimrc"
+	} >> "${HOMEDIR}/.vimrc"
+	echo Added stub to existing $HOMEDIR/.vimrc
+else
+	{
+		echo "source \"$SHATDIR/vim/vimrc\""
+	} >> "${HOMEDIR}/.vimrc"
+	echo Created $HOMEDIR/.vimrc
+fi
+
+if [ -e "${HOMEDIR}/.screenrc" ]
+then
+	{
+		echo '# Uncomment to enable'
+		echo '# '"source \"$SHATDIR/screen/screenrc\""
+	} >> "${HOMEDIR}"/.screenrc
+	echo Added stub to existing $HOMEDIR/.screenrc
+else
+	{
+		echo "source \"$SHATDIR/screen/screenrc\""
+	} >> "${HOMEDIR}"/.screenrc
+	echo Created $HOMEDIR/.screenrc
+fi
+
 
