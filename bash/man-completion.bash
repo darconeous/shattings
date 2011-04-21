5075ad4e8565868ff30ca0668f26af9525df5beb
Robert Quattlebaum <darco@deepdarc.com>
2011-04-21 15:51:26 -0700

    Added initial version of bash manpage autocompletion

---
+++ bash/man-completion.bash
@@ -0,0 +1,49 @@
+
+# usage: man [-adfhktwW] [section] [-M path] [-P pager] [-S list] [-m system] [-p string] name ...
+
+_man_complete() 
+{
+    local cur prev opts paths section
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+	opts="-a -d -f -h -k -t -w -W -M -P -S -M -p"
+
+	paths="`man -w`"
+	
+	#xargs -0 -n 1 printf "%s/man1\000" | 
+	
+
+
+	case "${cur}" in
+		-*)
+			COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+			return 0
+		;;
+		[123456789]*)
+			COMPREPLY=( $(compgen -W "1 2 3 4 5 6 7 8 9" -- ${cur}) )
+			return 0
+		;;
+		"")
+			if [ "$section" = ""]
+			then COMPREPLY=( $(compgen -W "${opts} 1 2 3 4 5 6 7 8 9" -- ${cur}) )
+			else COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+			fi
+			return 0
+		;;
+		*)
+			case "${prev}" in
+				-[MPSmp]) ;;
+				*)
+					COMPREPLY=( $(compgen -W "`tr ':\n' '\000' <<<$paths | xargs -0 -n 1 -J % find % -type f -name $cur'*' -exec basename {} ';' | sed 's/\([^.]*\)\..*$/\1/'`" -- ${cur}) )
+					return 0
+				;;
+			esac
+		;;
+
+	esac
+
+}
+complete -F _man_complete man
+
+
