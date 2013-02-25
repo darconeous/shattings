1be0097e8ce5a11bf4781338510b7865090d96c3
Robert Quattlebaum <darco@deepdarc.com>
2013-02-25 13:48:54 -0800

    ps1-git-svn: Use 'bold' instead of yellow for repo info

--- bash/ps1-git-svn.sh
+++ bash/ps1-git-svn.sh
@@ -104,10 +104,10 @@ function parse_svn_revision() {
 
 if [ `whoami` = root ]
 then
-	PS1_PREFIX='\[\033[1;31m\]\h:\[\033[1;34m\]\W \[\033[33m\]'
+	PS1_PREFIX='\[\033[1;31m\]\h:\[\033[1;34m\]\W \[\033[0;1m\]'
 	PS1_SUFFIX='\[\033[0m\]\$ '
 else
-	PS1_PREFIX='\[\033[1;32m\]\h:\[\033[1;34m\]\W \[\033[33m\]'
+	PS1_PREFIX='\[\033[1;32m\]\h:\[\033[1;34m\]\W \[\033[0;1m\]'
 	PS1_SUFFIX='\[\033[0m\]\$ '
 fi
 
